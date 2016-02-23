60723b3beb1407633017ed304ac050f0db028576
gatorlu <pengluustc@gmail.com>
2016-02-22 22:02:31 -0800

    change review class date to timestamp

--- src/main/java/coen268/com/ReviewResource.java
+++ src/main/java/coen268/com/ReviewResource.java
@@ -9,6 +9,60 @@ import javax.ws.rs.core.*;
 
 @Path("review")
 public class ReviewResource {
+	
+	// post a new review for a restaurant
+	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addReview(@FormParam("restaurant_id") int restaurant_id, @FormParam("review") String reviewTxt) {
+		
+		Connection conn = null;
+		PreparedStatement ps1 = null, ps2 = null;
+		ResultSet rs = null;
+		
+		Review review = null;
+		
+		try {
+			conn = DbUtil.getConnection();
+			// add new review
+			ps1 = conn.prepareStatement("insert into review (restaurant_id, review) values (?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps1.setInt(1, restaurant_id);
+			ps1.setString(2, reviewTxt);
+			
+			ps1.executeUpdate();
+			
+			// return newly add restaurant
+			ResultSet generatedKeys = ps1.getGeneratedKeys();
+			if (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                ps2 = conn.prepareStatement("select * from review where id=?");
+    			ps2.setInt(1, id);
+            }
+			
+			rs = ps2.executeQuery();
+			
+			if(rs.next()) {
+				review = new Review(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getTimestamp(4));
+			}
+			
+			return Response.status(200).entity(review).build();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(500).build();
+		}
+		finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps1 != null) ps1.close();
+				if(ps2 != null) ps2.close();
+				if(conn != null) conn.close();
+			}
+			catch(Exception ex){}
+		}
+		
+	}
 
 	// get all reviews for a restaurant
 	@GET
@@ -29,7 +83,7 @@ public class ReviewResource {
 			rs = ps.executeQuery();
 			
 			while(rs.next()) {
-				list.add(new Review(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getDate(4)));
+				list.add(new Review(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getTimestamp(4)));
 			}
 		}
 		catch(Exception e) {
@@ -52,14 +106,14 @@ class Review {
 	public int id=-1;
 	public int restaurant_id=-1;
 	public String review;
-	public Date date;
+	public Timestamp timestamp;
 	
 	public Review() {}
 	
-	public Review(int id, int restaurant_id, String review, Date date) {
+	public Review(int id, int restaurant_id, String review, Timestamp timestamp) {
 		this.id = id;
 		this.restaurant_id = restaurant_id;
 		this.review = review;
-		this.date = date;
+		this.timestamp = timestamp;
 	}
 }
